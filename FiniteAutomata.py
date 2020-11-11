bd5b896544c98e61339e9d97c57d20c10d5a8398
Bogdan Cristina <cristina.bogdan9996@yahoo.com>
2020-11-11 11:37:16 +0200

    Verify sequence

--- FiniteAutomata.py
+++ FiniteAutomata.py
@@ -1,10 +1,10 @@
 class FA:
     def __init__(self):
-        self.__states=[]
-        self.__alphabet=[]
-        self.__initial = []
+        self.__states = []
+        self.__alphabet = []
+        self.__initial = 0
         self.__final = []
-        self.__fa =[]
+        self.__fa = []
         self.readFromFile()
 
     def readFromFile(self):
@@ -17,8 +17,8 @@ class FA:
         self.__alphabet = f.readline().split(" ")
         self.__alphabet[-1] = self.__alphabet[-1].strip("\n")
 
-        self.__initial = f.readline().split(" ")
-        self.__initial[-1] = self.__initial[-1].strip("\n")
+        self.__initial = f.readline()
+        self.__initial = self.__initial.strip("\n")
 
         self.__final = (f.readline().split(" "))
         self.__final[-1] = self.__final[-1].strip("\n")
@@ -33,6 +33,7 @@ class FA:
         print("3 - Display transitions")
         print("4 - Display initial states")
         print("5 - Display final states")
+        print("6 - Verify if a sequence is accepted by the FA")
         print("0 - Quit")
         print()
 
@@ -54,8 +55,7 @@ class FA:
 
     def displayInititalSt(self):
         print("\nInitial states:\n")
-        for f in self.__initial:
-            print(f)
+        print(self.__initial)
 
     def displayFinalSt(self):
         print("\nFinal states:\n")
@@ -77,10 +77,62 @@ class FA:
                 self.displayInititalSt()
             elif com == "5":
                 self.displayFinalSt()
+            elif com == "6":
+                if (not self.isDeterministic()):
+                    print("FA is not deterministic")
+                    continue
+                seq = input("Enter sequence: ")
+                if(self.verifySequence(seq)):
+                    print("Sequence is valid")
+                else:
+                    print("Sequence is NOT valid")
             elif com == "0":
                 print("\nGoodbye! :)")
             else:
                 print("\nUnknown command")
 
+    def isDeterministic(self):
+        visited = []
+        for el in self.__fa:
+            if [el[0],el[1]] in visited:
+                return False
+            visited.append([el[0],el[1]])
+        return True
+
+    def getTransFforState(self,state):
+        all =[]
+        for el in self.__fa:
+            if el[0] == state:
+                all.append([el[1], el[2]])
+        return all
+
+    def verifySequence(self,sequence):
+        current = self.__initial
+        status = 1
+        index = 0
+        while True:
+            availableTrans = self.getTransFforState(current)
+            for a in availableTrans:
+                if sequence[index] == a[1]:
+                    status = 1
+                    current = a[0]
+                    index += 1
+                    break
+                else:
+                    status = 0
+            if status == 0:
+                return False
+            elif index == len(sequence)-1 and current in self.__final:
+                return True
+
+
 fa = FA()
-fa.start()
+
+#checking for a few cases
+print(fa.verifySequence("abcd")) #true
+print(fa.verifySequence("abcdddddd")) #true
+print(fa.verifySequence("bcd")) #true
+print(fa.verifySequence("abbcd")) #false
+print(fa.verifySequence("bbcd")) #false
+
+#fa.start()
